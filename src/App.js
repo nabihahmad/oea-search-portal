e8a214b807af5cfd602249df8fba9a2fdef74efc
Nabih <nabih.ahmad@hotmail.com>
2024-03-10 02:16:44 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import jsonData from './data/data.json';
 import './App.css';
 
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
+
+  const handleSearch = () => {
+    const result = jsonData.find(item => item.Code === parseInt(searchValue));
+    setSearchResult(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="center">
+        <input
+          className="input"
+          type="number"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder="Enter a number"
+          autoComplete="on" // Enable autocompletion
+          list="data-keys" // Specify the datalist ID for autocompletion
+        />
+        <datalist id="data-keys">
+          {jsonData.map((item, index) => (
+            <option key={index} value={item.Code} />
+          ))}
+        </datalist>
+        <button className="button" onClick={handleSearch}>Search</button>
+        {searchResult && (
+          <table className="table">
+            <tbody>
+              {Object.keys(searchResult).map(key => (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{searchResult[key]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 }
